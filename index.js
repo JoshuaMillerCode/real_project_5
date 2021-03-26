2042672f7d007314c3d72f718405fbeb5f8489a3
Joshua <millerjoshua737@gmail.com>
2021-03-25 23:35:43 -0500

    refractered with when property

--- index.js
+++ index.js
@@ -10,114 +10,67 @@ consola.info("Getting started using the api-starter-kit-package");
 
 const configVars = [];
 
-inquirer
-    .prompt([
-    {
-        name: 'apiProjectChoice',
-        type: 'list',
-        message: 'Please pick what API you would like to implement into your project. If your not ready to decide, dont worry just press CTRL + C to stop',
-        choices: ['nasa', 'marvel', 'movie', 'youtube',  'news']
-    }
-    ])
-    .then((apiChoice) => { 
-        configVars.push(apiChoice)
-        switch (apiChoice.apiProjectChoice) {
-            case "nasa":  
-                inquirer.prompt([
-                    {
-                        name: 'nasaChoice',
-                        type: 'list',
-                        message: "Which NASA api would you like to use?",
-                        choices: ['Picture of the day', 'Rovers']
-                    }
-                ])
-                .then(({nasaChoice}) => {
-                    if (nasaChoice === 'Rovers') {
-                        inquirer.prompt([
-                        {
-                            name: 'roverChoice',
-                            type: 'list',
-                            message: "Please pick which rover you would like to implement?",
-                            choices: ['Spirit', 'Curiosity', 'Opportunity', 'Perseverance']
-                        }
-                    ])
-                    .then(({roverChoice}) => {
-                        switch (roverChoice) {
-                            case 'Spirit':
-                                return {
-                                    roverChoice: roverChoice,
-                                    url: ''
-                                }
-                            case 'Curiosity':
-                                return {
-                                    roverChoice: roverChoice,
-                                    url: ''
-                                }
-                            case 'Opportunity':
-                                return {
-                                    roverChoice: roverChoice,
-                                    url: ''
-                                }
-                            case 'Perseverance':
-                                return {
-                                    roverChoice: roverChoice,
-                                    url: ''
-                                }
-                        }
-                    })
-                    } else {
-                        return {
-                            nasaChoice: nasaChoice,
-                            url: ''
-                        }
-                    }
-                    
-                })
-                break;
-            case 'marvel':
-                return console.info('OHHHHHH, now thats a good choice')
-            case 'movie':
-                return console.info('OHHHHHH, now thats a good choice')
-            case 'youtube':
-                return console.info('OHHHHHH, now thats a good choice')
-            case 'news':
-                return console.info('OHHHHHH, now thats a good choice')
 
-        } 
-        
-    })
-    .then((answer) => {
-        configVars.push(answer)
-        inquirer.prompt([
-            {
-                name: 'apiKey',
-                input: 'input',
-                message: 'Enter your api-key, if you have one already:',
-                validate: (input) => {
-                    if (/^([A-Za-z\-\_\d])+$/.test(input)) {
-                        return true;
-                    } else {
-                        return 'Project name must contain letters, numbers, underscores and hashes only.';
-                    }
-                },
-                
+(async () => {
+    await inquirer
+    .prompt([
+        {
+            name: 'apiProjectChoice',
+            type: 'list',
+            message: 'Please pick what API you would like to implement into your project. If your not ready to decide, dont worry just press CTRL + C to stop',
+            choices: ['nasa', 'marvel', 'movie', 'youtube',  'news']
+        },
+        {
+            name: 'apiChoice',
+            type: 'confirm',
+            message: 'Do you have an Api Key yet?',
+        },
+        {
+            name: 'apiKey',
+            input: 'input',
+            message: 'Enter your api-key, if you have one already:',
+            validate: (input) => {
+                if (/^([A-Za-z\-\_\d])+$/.test(input)) {
+                    return true;
+                } else {
+                    return 'Project name must contain letters, numbers, underscores and hashes only.';
+                }
             },
-            {
-                name: 'projectName',
-                type: 'input',
-                message: 'Name this Project:',
-                validate: (input) => {
-                    if (/^([A-Za-z\-\_\d])+$/.test(input)) {
-                        return true;
-                    } else {
-                        return 'Project name must contain letters, numbers, underscores and hashes only.';
-                    }
+            when: (answers) => answers.apiChoice === true
+        },
+        {
+            name: 'projectName',
+            type: 'input',
+            message: 'Name this Project:',
+            validate: (input) => {
+                if (/^([A-Za-z\-\_\d])+$/.test(input)) {
+                    return true;
+                } else {
+                    return 'Project name must contain letters, numbers, underscores and hashes only.';
                 }
             }
-        ])
-    })
+        },
+        {
+            name: 'nasaChoice',
+            type: 'list',
+            message: "Which NASA api would you like to use?",
+            choices: ['Picture of the day', 'Rovers'],
+            when: (answers) => answers.apiProjectChoice === 'nasa'
+        },
+        {
+            name: 'roverChoice',
+            type: 'list',
+            message: "Please pick which rover you would like to implement?",
+            choices: ['Spirit', 'Curiosity', 'Opportunity', 'Perseverance'],
+            when: (answers) => answers.nasaChoice === 'Rovers'
+        },
+        
+    ])
+    
     .then((answers) => {
-        configVars.push(answers)
+        // configVars.push(answers)
+        fs.writeFileSync(`${cwd}/config.json`, JSON.stringify(answers))
+        // return console.log(configVars)
     })
 
 
@@ -128,7 +81,8 @@ inquirer
     } else {
       // Something else went wrong
     }
-});
+})
+})();
 
 function createDirectoryContents (templatePath, newProjectPath) {
     const filesToCreate = fs.readdirSync(templatePath);

ebd71dc8af2b49fb9dc1f5fe510a502ebc245f30
Joshua <millerjoshua737@gmail.com>
2021-03-30 22:02:18 -0500

    a test was in the commit, oops

--- test-2/src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-    import { Link } from "react-router-dom";
-    export default function Home() {
-      return (
-        <div className="home">
-          <Link className="home-link" to="/${response.roverChoice}" ><h1>{response.roverChoice} Rover</h1></Link>
-        </div>
-      );
-    }
